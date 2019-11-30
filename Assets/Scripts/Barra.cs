958e3680b3184a78f381ee9ccbf6d6c7ca93de89
leeavemeal0ne <evishaemece@gmail.com>
2019-11-30 19:36:52 +0100

    Introducido prefab mesas y barra con UI

---
+++ Assets/Scripts/Barra.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Barra : MonoBehaviour
+{
+    private Canvas canvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        canvas = GetComponentInChildren<Canvas>();
+        Debug.Log(canvas);
+        canvas.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Hola colision");
+        if (collision.tag == "Player") {
+            canvas.enabled = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            canvas.enabled = false;
+        }
+    }
+}
